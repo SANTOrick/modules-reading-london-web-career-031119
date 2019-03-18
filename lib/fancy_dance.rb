29b49b0d8b4ec2841ebc18d23a426d76d5781fab
Riccardo <riccardo_london@live.it>
2019-03-18 10:52:03 +0000

    Automatically backed up by Learn

--- lib/fancy_dance.rb
+++ lib/fancy_dance.rb
@@ -5,4 +5,11 @@ class FancyDance
     return "This class produces objects that love to dance."
   end
 
+  module ClassMethods
+
+    def metadata
+      return "This class produces objects that love to dance."
+
+    end
+
 end
