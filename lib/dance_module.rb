6717898134069d270c68c056b54cb36abdde83cd
Riccardo <riccardo_london@live.it>
2019-03-18 10:55:13 +0000

    Automatically backed up by Learn

--- lib/dance_module.rb
+++ lib/dance_module.rb
@@ -1,5 +1,5 @@
 module Dance
-  module InstanceMethods
+
   def twirl
     return "I'm twirling!"
   end
@@ -15,5 +15,7 @@ module Dance
   def take_a_bow
     return "Thank you, thank you. It was a pleasure to dance for you all."
   end
+  module InstanceMethods
+
 end
 end
